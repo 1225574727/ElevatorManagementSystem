5df110e2a6908872475ee9dbd3bd90399e8679b8
liumingfei_yitong <lmf@yitong.com.cn>
2022-01-17 19:33:52 +0800

    文件上传 调整

--- ElevatorManagementSystem/Tools/Upload/VideoUpload/EMBackgroundService.swift
+++ ElevatorManagementSystem/Tools/Upload/VideoUpload/EMBackgroundService.swift
@@ -11,10 +11,11 @@ import MBProgressHUD
 import UIKit
 import Alamofire
 import Moya
+import HandyJSON
 
 /// 文件上传地址
 let uploadFileUrl = PCDBaseURL + "/upload/public/breakpointUpload"
-let uploadUnitSize = 1 * 1024 * 1024/5
+let uploadUnitSize = 1 * 1024 * 1024
 let EMBoundary = "BoundaryForEMSystem"
 
 enum EMUploadResult {
@@ -23,9 +24,6 @@ enum EMUploadResult {
 }
 
 class EMBackgroundService: NSObject,URLSessionTaskDelegate,URLSessionDataDelegate {
-    
-//    后台任务
-//    var backgroundTask: URLSessionUploadTask!
 	
 	/// 服务器返回数据
 	var response: NSMutableData!
@@ -54,55 +52,14 @@ class EMBackgroundService: NSObject,URLSessionTaskDelegate,URLSessionDataDelegat
 			NSLog("上传任务为空")
 		}
 	}
-	
-	/// 初始化backgoundSession
-//	func setupBackgroundSession() {
-//		let now = Date()
-//		let dateFormatter = DateFormatter()
-//		dateFormatter.dateFormat = "yyyyMMddHHMMSS"
-//		let dateStr = dateFormatter.string(from: now)
-//
-//		let randomIndex = Int(arc4random()%10000)+1
-//		let configIndentifier = "EM" + dateStr + String(randomIndex)
-//
-//		let configration = URLSessionConfiguration.background(withIdentifier: configIndentifier)
-//		/// 设置超时时间
-//		configration.timeoutIntervalForResource = 12*60*60
-//		backgoundSession = URLSession(configuration: configration, delegate: self, delegateQueue: OperationQueue.main)
-//	}
-	
-	private func startUploadFile() {
 		
-//		setupBackgroundSession()
-
+	private func startUploadFile() {
+	
 		model.status = .EMUploading
 		
 		/// 上传代码
-//		perform(#selector(uploadUnitWith), with: nil, afterDelay: 2)
-		
 		uploadUnitWith()
 	}
-    
-    lazy var backUploadSession: URLSession = {
-            //只执行一次
-//            let now = Date()
-//            let dateFormatter = DateFormatter()
-//            dateFormatter.dateFormat = "yyyyMMddHHMMSS"
-//            let dateStr = dateFormatter.string(from: now)
-//
-//            let randomIndex = Int(arc4random()%10000)+1
-//            let configIndentifier = "EM" + dateStr + String(randomIndex)
-//
-//            let configration = URLSessionConfiguration.background(withIdentifier: configIndentifier)
-//		let queue = OperationQueue()
-//		queue.maxConcurrentOperationCount = 100
-//		    let currentSession = URLSession(configuration: configration, delegate: self, delegateQueue: queue)
-		    let config = URLSessionConfiguration.background(withIdentifier:
-					"background-session")
-			let currentSession = URLSession(configuration: config, delegate: self,
-												delegateQueue: nil)
-            return currentSession
-    }()
 	
 	@objc func uploadUnitWith() {
 		
@@ -127,8 +84,11 @@ class EMBackgroundService: NSObject,URLSessionTaskDelegate,URLSessionDataDelegat
 		for key in keys {
 			requestPath = requestPath + "\(key)=\(params[key]!)&"
 		}
-		
-		let upLoadRequest = Alamofire.AF.upload(unitData, to: requestPath)
+		//        uploadData.append("Content-Disposition: form-data; name=\"file\"; filename=\"\(unitData.MD5().hexString()).tmp\"\r\n".data(using: .utf8)!) //multipartFile
+//		let upLoadRequest = Alamofire.AF.upload(unitData, to: requestPath)
+		let upLoadRequest = Alamofire.AF.upload(multipartFormData: { formData in
+			formData.append(unitData, withName: "file", fileName: "\(unitData.MD5().hexString()).tmp", mimeType: "application/octet-stream")
+		}, to: requestPath)
 		upLoadRequest.response { [weak self] data in
 			guard let self = self else {
 				return
@@ -139,7 +99,15 @@ class EMBackgroundService: NSObject,URLSessionTaskDelegate,URLSessionDataDelegat
 				self.completeHandler?(.error(error))
 				self.model.status = .EMUploadFailed
 			} else {
+				
 				if self.model.uploadCount + 1 == self.model.totalCount {
+					if case let .success(response) = data.result {
+						
+						if response != nil {
+							let json = JSON(response)
+							print("response:\(json)")
+						}
+					}
 					// 如何获取data中后台返回的信息
 					self.model.status = .EMUploaded
 					
